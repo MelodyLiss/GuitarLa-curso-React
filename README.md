e469488555f7b3b4538fb05f45d3b357526c6ec7
MelodyLiss <lisette.jofre.1@gmail.com>
2024-12-29 15:38:24 -0300

    sumar valor ok

---
+++ README.md
@@ -0,0 +1,10 @@
+# GuitarLa 🎸🎼
+
+## Primeros pasos en construcción de componentes y hook 😉🍊🧡
+
+- Creación base de tienda virtual de guitarras , con carrito de compras.
+- Realización de componente para la visualización de las guitarras
+- Incorporación de hook,
+
+### **Este proyecto es parte del curso práctico de React de Juan de la Torre.**
+
---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+import { Header } from './components/Header'
+import { Guitar } from './components/Guitar'
+import { db } from './data/db'
+
+function App() {
+
+  const [data, setData] = useState(db)
+  const [cart,setCart] = useState([])
+
+  const addToCart = (item) =>{
+
+    const itemExiste = cart.findIndex(guitar => guitar.id === item.id)
+
+    if(itemExiste >=0){
+      const updateCart = [...cart]
+      updateCart[itemExiste].quantity++
+    }else{
+      item.quantity = 1
+      setCart([...cart,item])
+    }
+  }
+
+  return (
+    <>
+      <Header 
+      cart={cart}
+      />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+
+        {data.map((guitar)=>( 
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
+        </div>
+      </main>
+
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
